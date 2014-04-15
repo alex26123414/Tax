457ccc4b7e3050b7f180b9c15c54a023a2018f57
Sabri <Sabri@Sabri>
2014-04-15 17:49:59 +0200

    Php project with html file

---
+++ databaseConnector.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+<?php
+
+    $username = "root";
+    $password = "";
+    $hostname = "localhost";
+    
+
+    //connection to the database
+    $dbhandle = mysql_connect($hostname, $username, $password)
+    or die("Unable to connect to MySQL");
+    echo "Connected to MySQL<br>";
+
+    //select a database to work with
+    $selected = mysql_select_db("skat",$dbhandle)
+    or die("Could not select examples");
+
+    //execute the SQL query and return records
+    $result = mysql_query("SELECT * FROM tax");
+    while($row = mysql_fetch_array($result)) {
+    echo $row['idtax'] . " " . $row['value'];
+    echo "<br>";
+  }
+
+    //close the connection
+    mysql_close($dbhandle);
+
+?>
+    </body>
+</html>
